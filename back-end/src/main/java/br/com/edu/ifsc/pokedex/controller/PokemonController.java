3addfd24b9d1b0bc5fb2ddf6d984fab10911e702
Keila <keiladaianefernandes24@gmail.com>
2019-04-22 20:38:45 -0300

    Método DELETE e PUT do pokemon criado

--- back-end/src/main/java/br/com/edu/ifsc/pokedex/controller/PokemonController.java
+++ back-end/src/main/java/br/com/edu/ifsc/pokedex/controller/PokemonController.java
@@ -3,9 +3,11 @@ package br.com.edu.ifsc.pokedex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +32,7 @@ public class PokemonController {
 	@Autowired
 	PokemonRepository pokemonRepository;
 	
+	
 	/* Lista todos os pokemons salvos no banco de dados
 	 * em formato JSON.
 	 */
@@ -37,6 +40,7 @@ public class PokemonController {
 	public List<Pokemon> listPokemons() {
 		return pokemonRepository.findAll();
 	}
+	
 	
 	/* Lista o pokemon específico pelo id que está salvo 
 	 * no banco de dados, em formato JSON,.
@@ -46,13 +50,33 @@ public class PokemonController {
 		return pokemonRepository.findById(id);
 	}
 	
+	
 	/* Vai receber através do corpo da requisição o pokemon
 	 * e vai ser salvo no banco de dados.
 	 */
 	@PostMapping("/pokemon")
 	public Pokemon savePokemon(@RequestBody Pokemon pokemon) {
 		return pokemonRepository.save(pokemon);
-		
+	}
+	
+	
+	/* Irá deletar um pokemon em uma determinada posição
+	 * conhecida como id.
+	 * Irá receber como parâmetro o pokemon a ser deletado
+	 * em formato JSON, no corpo da requisição.
+	 */
+	@DeleteMapping("/pokemon")
+	public void deletePokemon(@RequestBody Pokemon pokemon) {
+		pokemonRepository.delete(pokemon);
+	}
+	
+	
+	/* Irá atualizar as informações de um pokemon através das
+	 * informações recebidas do RequestBody em formato JSON.
+	 */
+	@PutMapping("/pokemon")
+	public Pokemon updatePokemon(@RequestBody Pokemon pokemon) {
+		return pokemonRepository.save(pokemon);
 	}
 	
 }
